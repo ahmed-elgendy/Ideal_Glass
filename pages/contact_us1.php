1f08b580e07f18f8c8b35c4e2998cf50bf04b287
Ahmed Elgendy <develgendy@gmail.com>
2021-02-09 02:36:43 +0200

    edit coode

---
+++ pages/contact_us1.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Ideal_Glass</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="../img/2.png" rel="icon">
+  <link href="../img/2.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="../lib/nivo-slider/css/nivo-slider.css" rel="stylesheet">
+  <link href="../lib/owlcarousel/owl.carousel.css" rel="stylesheet">
+  <link href="../lib/owlcarousel/owl.transitions.css" rel="stylesheet">
+  <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../lib/animate/animate.min.css" rel="stylesheet">
+  <link href="../lib/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Nivo Slider Theme -->
+  <link href="../css/nivo-slider-theme.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="../css/style.css" rel="stylesheet">
+
+  <!-- Responsive Stylesheet File -->
+  <link href="../css/responsive.css" rel="stylesheet">
+
+  <!-- =======================================================
+  
+
+  ======================================================= -->
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+
+  <div id="preloader"></div>
+
+  <header>
+    <!-- header-area start -->
+    <div id="sticker" class="header-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-default">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".bs-example-navbar-collapse-1" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+                <!-- Brand -->
+                <a class="navbar-brand page-scroll sticky-logo" href="../index.html">
+                  <h1><span>IDEAL</span>GLASS</h1>
+               
+								</a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse main-menu bs-example-navbar-collapse-1" id="navbar-example">
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="active">
+                    <a class="page-scroll" href="../index.html">Home</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="About_us.html">About</a>
+                  </li>
+                  <li>
+                    <a class="page-scroll" href="#contact">Contact</a>
+                  </li>
+                </ul>
+              </div>
+              <!-- navbar-collapse -->
+            </nav>
+            <!-- END: Navigation -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- header-area end -->
+  </header>
+
+  <!-- header end -->
+
+
+
+
+
+
+   <!-- Start Bottom Header -->
+   <div class="header-bg page-area">
+    <div class="home-overly"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="slider-content text-center">
+            <div class="header-bottom">
+              <div class="layer2 wow zoomIn" data-wow-duration="1s" data-wow-delay=".4s">
+                <h1 class="title2">Welcome to</h1>
+              </div>
+              <div class="layer3 wow zoomInUp" data-wow-duration="2s" data-wow-delay="1s">
+                <h2 class="title3">contact us page</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- END Header -->
+  <!-- Start contact Area -->
+  <div id="contact" class="contact-area">
+    <div class="contact-inner area-padding">
+      <div class="contact-overly"></div>
+      <div class="container ">
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="section-headline text-center">
+              <h2>Contact us</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- Start contact icon column -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-mobile"></i>
+                <p>
+                  Call: +201099230400<br>
+                  Call: +201097636316<br>
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- Start contact icon column -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-envelope-o"></i>
+                <p>
+                  Email: info@idealglass.company <br>
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- Start contact icon column -->
+          <div class="col-md-4 col-sm-4 col-xs-12">
+            <div class="contact-icon text-center">
+              <div class="single-icon">
+                <i class="fa fa-map-marker"></i>
+                <p>
+                  Building 103 Masaken El Dobat Zahraa Nasr City                 
+                
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+
+          <!-- Start Google Map -->
+          <div class="col-md-6 col-sm-6 col-xs-12">
+            <!-- Start Map -->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <!-- End Map -->
+          </div>
+          <!-- End Google Map -->
+          <!-- Start  contact -->
+          
+ <!-- Start  contact -->
+ <div class="col-md-6 col-sm-6 col-xs-12">
+  <div class="form contact-form"  >
+    <div id="sendmessage">Your message has been sent. Thank you!</div>
+    <div id="errormessage"></div>
+    <form action="" method="post" role="form" class="contactForm">
+      <div class="form-group">
+        <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+        <div class="validation"></div>
+      </div>
+      <div class="form-group">
+        <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+        <div class="validation"></div>
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+        <div class="validation"></div>
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+        <div class="validation"></div>
+      </div>
+      <div class="text-center"><button type="submit">Send Message</button></div>
+    </form>
+  </div>
+</div>
+<!-- End Left contact -->
+          <!-- End Left contact -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Contact Area -->
+
+ <!-- Start Footer bottom Area -->
+ <footer>
+  <div class="footer-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+          <div class="footer-content">
+            <div class="footer-head">
+              <div class="footer-logo">
+                <h2><span>IDEAL</span>GLASS</h2>
+              </div>
+
+              <p>Specialized in manufacturing and installing <br> [sliding and hinged doors - handrails - hinged sliding shower cabins - wall,
+                ceiling and bar cladding - facade floors - fixed and mobile partitions]</p>
+              <div class="footer-icons">
+                <ul>
+                  <li>
+                    <a target="_blank" href="https://web.facebook.com/idealglasseg"><i class="fa fa-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a   target="_blank" href="https://web.facebook.com/messages/t/253842665670422"><i class="fa fa-commenting"></i></a>
+                  </li>
+                  <li>
+                    <a target="_blank" href="mailto:info@idealglass.company"><i class="fa fa-envelope-o"></i></a>
+                  </li>
+                 
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end single footer -->
+        <div class="col-md-4 col-sm-4 col-xs-12">
+          <div class="footer-content">
+            <div class="footer-head">
+              <h4>Contact Us</h4>
+              
+              <div class="footer-contacts">
+                <p><span > Tel:<a href="tel:+201099230400">+201099230400</a>  </span> </p>
+                <p><span > Tel:<a href="tel:+201097636316">+201097636316</a>  </span> </p>
+
+                <p><span>Email:</span>info@idealglass.company</p>
+                <p><span>Working Hours:</span> 9am-5pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end single footer -->
+        <div class="col-md-4 col-sm-4 col-xs-12">
+          <div class="footer-content">
+            <div class="footer-head">
+              <h4>Same Photo</h4>
+              <div class="flicker-img">
+                <a href="#"><img src="../img/screenn.jpeg" alt=""></a>
+                <a href="#"><img src="../img/scren.jpeg" alt=""></a>
+                <a href="#"><img src="../img/screenn.jpeg" alt=""></a>
+                <a href="#"><img src="../img/intarfac.jpg" alt=""></a>
+                <a href="#"><img src="../img/ab.png" alt=""></a>
+                <a href="#"><img src="../img/shawar1.jpg" alt=""></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-area-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+          <div class="copyright text-center">
+            <p>
+              &copy; Copyright <strong>A_Elgendy</strong>. All Rights Reserved
+            </p>
+          </div>
+          <div class="credits">
+            Designed by <a target="_blank" href="https://cutt.us/DPa90">A_Elgendy</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="../lib/venobox/venobox.min.js"></script>
+  <script src="../lib/knob/jquery.knob.js"></script>
+  <script src="../lib/wow/wow.min.js"></script>
+  <script src="../lib/parallax/parallax.js"></script>
+  <script src="../lib/easing/easing.min.js"></script>
+  <script src="../lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+  <script src="../lib/appear/jquery.appear.js"></script>
+  <script src="../lib/isotope/isotope.pkgd.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+
+  <script src="../js/main.js"></script>
+</body>
+
+</html>
